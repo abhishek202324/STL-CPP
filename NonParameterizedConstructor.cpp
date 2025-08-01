e85a9819ac280ab9dfb634645e336d25a62208dd
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-08-01 19:24:15 +0530

    Add files via upload

---
+++ NonParameterizedConstructor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class hello{
+    public:
+    hello(){
+        cout<<"Hello My name is Abhishek"<<endl;
+    }
+
+};
+int main()
+    {
+        hello s1;
+        return 0;
+    }
